1393bf88b14ca650cb40dc934f964897f6ad2119
Shojjoti <shojjotihhossenbd30@gmail.com>
2025-11-15 14:37:06 +0600

    JavaScript Add

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,30 +3,31 @@ function loginUser() {
     let nameValue = document.getElementById("name").value;
     let passwordValue = document.getElementById("password").value;
 
-    // Name Validation
-    if (!nameValue || nameValue === "") {
+    // Name validation
+    if (nameValue === "") {
         document.getElementById("nameErr").innerHTML = "User Name is required";
     } else {
         document.getElementById("nameErr").innerHTML = "";
     }
 
-    // Password Validation
-    if (!passwordValue || passwordValue === "") {
+    // Password validation
+    if (passwordValue === "") {
         document.getElementById("passwordErr").innerHTML = "Password is required";
     } else {
         document.getElementById("passwordErr").innerHTML = "";
     }
-
-    // Check both fields filled
-    if (nameValue === "" || passwordValue === "") {
+ 
+    if (nameValue === "" || passwordValue === "") 
         return;
-    }
+
+    
 
     let correctUser = "Shojjoti";
     let correctPass = "Sh1234";
 
     if (nameValue === correctUser && passwordValue === correctPass) {
         alert("Login Successful!");
+        window.location.href = "dashboard.html";
     } else {
         alert("Invalid Username or Password!");
     }
